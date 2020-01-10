111c9417e86c0a5f751f73f175df019d6f3c6004
MarcBrejner <marc@kaabrejner.dk>
2020-01-10 15:07:09 +0100

    Updated Ability and Pokemon

--- src/Ability.java
+++ src/Ability.java
@@ -1,4 +1,45 @@
-public interface Ability {
-    public void damage();
-    public void restore();
+public class Ability{
+    private boolean onEnemy, onOwn;
+    private int cost;
+    private String name, ele;
+
+    public Ability(String name){
+        this.name = name;
+        switch(name){
+            case "Bolt":
+                bolt();
+                break;
+
+            case "Recover":
+                recover();
+                break;
+
+            case "Slap":
+                slap();
+                break;
+        }
+    }
+
+    public void bolt(){
+        onEnemy = true;
+        onOwn = false;
+        cost = 2;
+        ele = "Electric";
+    }
+
+    public void slap(){
+        onEnemy = true;
+        onOwn = false;
+        cost = 1;
+        ele = "Normal";
+    }
+
+    public void recover(){
+        onEnemy = false;
+        onOwn = true;
+        cost = 3;
+        ele = "Normal";
+
+    }
+
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,16 +1,73 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSerializer;
+
+import java.io.Serializable;
 import java.util.List;
 
 public class Pokemon {
-    public static int HP;
-    public static String name,ele;
-    public static List<Ability> abilityList;
-    public static boolean alive;
+    private int HP;
+    private String name,ele;
+    private List<Ability> abilityList;
+    private boolean alive;
 
-    public Pokemon(String name, String ele){
-        HP = 80;
-        this.name = name;
-        this.ele= ele;
+    public Pokemon(String name){
+        switch(name){
+            case "Pikachu":
+                pikachu();
+                this.name = name;
+                break;
+
+            case "Charmander":
+                charmander();
+                break;
+        }
+    }
+
+    private void pikachu(){
+        this.HP = 80;
+        this.ele = "Electric";
+        this.alive = true;
+    }
+
+    private void charmander(){
+        HP = 90;
+        ele = "Fire";
+        abilityList.add(new Ability("Slap"));
         alive = true;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEle() {
+        return ele;
+    }
+
+    public List<Ability> getAbilityList() {
+        return abilityList;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public static String toJson(Pokemon pokemon){
+        Gson gson = new Gson();
+        String json = gson.toJson(pokemon);
+        return json;
+    }
+
+    public static Pokemon fromJson(String json){
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Pokemon gsonPokemon = gson.fromJson(json, Pokemon.class);
+        return gsonPokemon;
     }
 
 }
